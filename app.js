4110c00fcc4c389479a1c8f5dbf1dff70bc470fc
Raghunandan Saraswat <nandanraghu276@gmail.com>
2024-08-18 01:43:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/user')
+const contactRoute = require('./routes/contact')
+
+
+mongoose.connect('mongodb+srv://nandanraghu276:Raghu12@vkr.gp71v.mongodb.net/?retryWrites=true&w=majority&appName=vkr')
+.then(res=>{console.log("connected to database")})
+.catch(err=>{console.log(err)})
+
+app.use(bodyParser.json())
+
+app.use('/user',userRoute)
+app.use('/contact',contactRoute)
+
+
+app.use('*',(req,res)=>{
+    res.status(404).json({
+        msg:'bad request'
+    })
+})
+
+module.exports = app
